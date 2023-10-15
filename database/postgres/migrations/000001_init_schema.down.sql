d57842430428545274d893b7be139d78e1f56963
Anil Khadka <anil.khadka3777@gmail.com>
2023-10-15 21:06:11 +0545

    Fixing Migration Down Setup

--- database/postgres/migrations/000001_init_schema.down.sql
+++ database/postgres/migrations/000001_init_schema.down.sql
@@ -1,7 +1,7 @@
-DROP TABLE users;
-DROP TABLE events;
-DROP TABLE event_roles;
 DROP TABLE event_organizers;
+DROP TABLE event_roles;
 DROP TABLE event_attendees;
 DROP TABLE event_sessions;
 DROP TABLE expenses;
+DROP TABLE events;
+DROP TABLE users;
